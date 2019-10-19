f84f7eb7eef584fa5053586aefbf0b02f51af2d7
“reddrum” <anton.kranz@me.com>
2019-10-19 23:50:45 +0300

    Added account settings

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def profile_picture account, width = 100, height = 100
+    thumb = account.image.present? ? account.image.thumb.url : "placeholder.jpg"
+    image_tag thumb, width: width, height: height, class: "profile-pic img-circle"
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_17_203523) do
+ActiveRecord::Schema.define(version: 2019_10_19_202048) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,8 @@ ActiveRecord::Schema.define(version: 2019_10_17_203523) do
     t.datetime "last_sign_in_at"
     t.inet "current_sign_in_ip"
     t.inet "last_sign_in_ip"
+    t.string "image"
+    t.string "company"
     t.index ["email"], name: "index_accounts_on_email", unique: true
     t.index ["reset_password_token"], name: "index_accounts_on_reset_password_token", unique: true
   end
