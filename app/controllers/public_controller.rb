0a2ef823a1bbc880017e6a06ebab5952424d1f6d
“reddrum” <anton.kranz@me.com>
2019-10-23 23:53:15 +0300

    Added notifications

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,5 +1,9 @@
 class PublicController < ApplicationController
   def main
+    if account_signed_in?
+      redirect_to dashboard_path, flash: {success: "Welcome to Realty finder!"} and return
+    end
+    
     @realties = Realty.latest
   end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,6 +12,12 @@ window.$ = window.jQuery = jQuery
 require("bootstrap")
 require("@fortawesome/fontawesome-free")
 
+import toastr from 'toastr';
+toastr.options = {
+  progressBar: true
+}
+global.toastr = toastr;
+
 require("./includes/realties");
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
